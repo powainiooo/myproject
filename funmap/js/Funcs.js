6133930867602acc528878255f25379fb567785b
gogotsj <powainiooo@163.com>
2017-05-11 01:22:17 +0800

    funmap

--- funmap/js/Funcs.js
+++ funmap/js/Funcs.js
@@ -4,60 +4,37 @@ var LoadingDoufen = (function(){
         base(this,LSprite,[]);
         var s = this;
 
-        s.graphics.drawRect(0,"#fff100",[0,0,750,1125],true,"#fff100");
+        s.graphics.drawRect(0,"#fff100",[0,0,750,1217],true,"#000");
 
-        s.line1 = new LSprite();
-        s.line1.pic = new LBitmap(new LBitmapData(loadinglist['L_line']));
-        s.line1.pic.x = -10;
-        s.line1.pic.y = -55;
-        s.line1.addChild(s.line1.pic);
-        s.line1.x = 200;
-        s.line1.y = 520;
-        s.line1.rotate = -45;
-        s.addChild(s.line1);
-        LTweenLite.to(s.line1,0.3,{rotate:-30,loop:true,ease:LEasing.None.easeIn}).to(s.line1,0.3,{rotate:-45,loop:true,ease:LEasing.None.easeIn});
+        s.line = new LBitmap(new LBitmapData(loadinglist["L_line"]));
+        s.line.x = -220;
+        s.line.y = 400;
+        s.addChild(s.line);
 
-        s.line2 = new LSprite();
-        s.line2.pic = new LBitmap(new LBitmapData(loadinglist['L_line']));
-        s.line2.pic.x = -10;
-        s.line2.pic.y = -55;
-        s.line2.addChild(s.line2.pic);
-        s.line2.x = 230;
-        s.line2.y = 520;
-        s.line2.rotate = 45;
-        s.addChild(s.line2);
-        LTweenLite.to(s.line2,0.3,{rotate:30,loop:true,ease:LEasing.None.easeIn}).to(s.line2,0.3,{rotate:45,loop:true,ease:LEasing.None.easeIn});
+        s.cover = new LBitmap(new LBitmapData(loadinglist["L_loading"]));
+        s.cover.x = 175;
+        s.cover.y = 400;
+        s.addChild(s.cover);
 
+        var cover = new LSprite();
+        cover.graphics.drawRect(1,'#000',[0,0,400,36],true,'#000');
+        cover.x = 175;
+        cover.y = 400;
+        s.line.mask = cover;
 
-        s.pic = new LBitmap(new LBitmapData(loadinglist['L_loading']));
-        s.pic.x = 94;
-        s.pic.y = 500;
-        s.addChild(s.pic);
-
-        s.circle = new LSprite();
-        s.circle.pic = new LSprite();
-        s.circle.pic.graphics.drawArc(0,'#fff',[0,0,55,0,2*Math.PI],true,"#fff100");
-        s.circle.addChild(s.circle.pic);
-        s.circle.x = 212;
-        s.circle.y = 562;
-        s.circle.scaleX = 0.55;
-        s.circle.scaleY = 0.55;
-        s.addChild(s.circle);
-        LTweenLite.to(s.circle,0.5,{scaleX:1,scaleY:1,loop:true,ease:LEasing.None.easeInOut}).to(s.circle,0.5,{scaleX:0.55,scaleY:0.55,loop:true,ease:LEasing.None.easeInOut});
-
-        s.circle2 = new LSprite();
-        s.circle2.pic = new LSprite();
-        s.circle2.pic.graphics.drawArc(0,'#fff',[0,0,37,0,2*Math.PI],true,"#231815");
-        s.circle2.addChild(s.circle2.pic);
-        s.circle2.x = 212;
-        s.circle2.y = 562;
-        s.circle2.scaleX = 0.2;
-        s.circle2.scaleY = 0.2;
-        s.addChild(s.circle2);
-        LTweenLite.to(s.circle2,0.6,{scaleX:1,scaleY:1,loop:true,ease:LEasing.None.easeInOut}).to(s.circle2,0.4,{scaleX:0.65,scaleY:0.65,loop:true,ease:LEasing.None.easeInOut}).to(s.circle2,0.4,{scaleX:1,scaleY:1,loop:true,ease:LEasing.None.easeInOut}).to(s.circle2,0.6,{scaleX:0.2,scaleY:0.2,loop:true,ease:LEasing.None.easeInOut});
+        s.txt = new LTextField();
+        s.txt.text = '0%';
+        s.txt.color = '#fff';
+        s.txt.x = (LGlobal.width - s.txt.getWidth())/2;
+        s.txt.y = 460;
+        s.txt.size = 28;
+        s.addChild(s.txt);
     }
     LoadingDoufen.prototype.setProgress = function(value){
         var s = this;
+        s.line.x = -220 + (value/100)*400;
+        s.txt.text = parseInt(value)+'%';
+        s.txt.x = (LGlobal.width - s.txt.getWidth())/2;
     };
     return LoadingDoufen;
 })();
@@ -104,3 +81,53 @@ function scaleIn(){
     self.scaleY = 1.4;
     LTweenLite.to(self,t,{scaleX:1,scaleY:1,alpha:1,ease:LEasing.Cubic.easeOut});
 }
+
+//打字
+function typeC(id,callback){
+    var arr;
+    if(id=='txt1'){
+        arr = ['英','雄','，','终','于','找','到','你','了','！','<br>','别','问','我','是','谁','，','带','上','你','的','速','度','与','激','情','，'];
+    }else{
+        arr = ['马','上','出','发','吧','！'];
+    }
+    var len = arr.length,htmlArr = [],i=0;
+    var s = document.getElementById(id);
+    s.style.display =  'block';
+    var t = setInterval(function(){
+        if(i<len){
+            htmlArr.push(arr[i]);
+            i++;
+            s.innerHTML = htmlArr.join("");
+        }else{
+            clearInterval(t);
+            callback();
+        }
+    },200)
+}
+
+//汽车
+function Car(){
+    base(this,LSprite,[]);
+    var self = this;
+
+    var bg = new LBitmap(new LBitmapData(imglist["car"]));
+    self.addChild(bg);
+
+    self.wheel = new LSprite();
+    self.wheel.pic = new LBitmap(new LBitmapData(imglist["icons"],0,544,112,112));
+    self.wheel.pic.x = -55;
+    self.wheel.pic.y = -57;
+    self.wheel.addChild(self.wheel.pic);
+    self.wheel.x = 163;
+    self.wheel.y = 172;
+    self.addChild(self.wheel);
+
+    self.wheel2 = self.wheel.clone();
+    self.wheel2.x = 588;
+    self.wheel2.y = 176;
+    self.addChild(self.wheel2);
+}
+Car.prototype.move = function(){
+    var self = this;
+    LTweenLite.to(self.wheel,0.4,{rotate:360,loop:true,ease:LEasing.None.easeIn}).to(self.wheel,0,{rotate:0,loop:true,ease:LEasing.None.easeIn});
+};
--- funmap/js/game.js
+++ funmap/js/game.js
@@ -2,13 +2,27 @@
 
 var loadinglist = {};
 var loadingData = new Array(
-    {name:"L_loading",path:imgsrc+"./images/loading.png"},
-    {name:"L_line",path:imgsrc+"./images/L_line.png"}
+    {name:"L_loading",path:imgsrc+"./images/loading_cover.png"},
+    {name:"L_line",path:imgsrc+"./images/loading_line.png"}
 );
 
 var imglist = {};
 var imgData = new Array(
-    {name:"logo",path:imgsrc+"./images/logo.png"}
+    {name:"p1_bg_normal",path:imgsrc+"./images/p1_bg_normal.jpg"},
+    {name:"p1_bg_ab",path:imgsrc+"./images/p1_bg_ab.jpg"},
+    {name:"p2_map_bg",path:imgsrc+"./images/p2_map_bg.jpg"},
+    {name:"p2_map_cover",path:imgsrc+"./images/p2_map_cover.jpg"},
+    {name:"p3_pic1",path:imgsrc+"./images/p3_pic1.jpg"},
+    {name:"p3_pic2",path:imgsrc+"./images/p3_pic2.jpg"},
+    {name:"p3_pic3",path:imgsrc+"./images/p3_pic3.jpg"},
+    {name:"p4_pic1",path:imgsrc+"./images/p4_pic1.jpg"},
+    {name:"p4_pic2",path:imgsrc+"./images/p4_pic2.jpg"},
+    {name:"p5_pic1",path:imgsrc+"./images/p5_pic1.jpg"},
+    {name:"p6_pic1",path:imgsrc+"./images/p6_pic1.jpg"},
+    {name:"helicopter",path:imgsrc+"./images/helicopter.png"},
+    {name:"car",path:imgsrc+"./images/car.png"},
+    {name:"c_imgs",path:imgsrc+"./images/c_imgs.png"},
+    {name:"icons",path:imgsrc+"./images/icons.png"}
 );
 
 var gameData = {};
@@ -55,27 +69,209 @@ function loading_gameData(){
             imglist = result;
             stage.removeChild(loadinglayer);
             loadinglayer = null;
-            page1Init();
+            page6Init();
         }
     );
 }
 
 function page1Init(){
     page1Layer = new LSprite();
-    page1Layer.graphics.drawRect(0,"#fff100",[0,0,750,1125],true,"#fff100");
     stage.addChild(page1Layer);
 
-    var list = LGlobal.divideCoordinate(1957, 664, 1, 11);
-    var data = new LBitmapData(imglist["logo"]);
-    var logo = new LAnimationTimeline(data, list);
-    logo.x = 286;
-    logo.y = 230;
-    logo.speed = 8;
-    logo.setLabel("end",0,10,1,false);
-    logo.addFrameScript("end",function(){
-        logo.stop();
-        logo.alpha = 0;
-        scaleIn.call(logo);
+    var bgNor = new LBitmap(new LBitmapData(imglist["p1_bg_normal"]));
+    page1Layer.addChild(bgNor);
+
+    var bgAb = new LBitmap(new LBitmapData(imglist["p1_bg_ab"]));
+    bgAb.alpha = 0;
+    page1Layer.addChild(bgAb);
+
+
+    var btnNext = new LButton(new LBitmap(new LBitmapData(imglist["icons"],0,1457,118,118)));
+    btnNext.x = 440;
+    btnNext.y = 740;
+    btnNext.alpha = 0;
+    page1Layer.addChild(btnNext);
+
+    btnNext.addEventListener(LMouseEvent.MOUSE_DOWN,function(){
+        document.getElementById("txt1").style.display = 'none';
+        document.getElementById("txt2").style.display = 'none';
+        stage.removeChild(page1Layer);
+        page1Layer = null;
     });
-    page1Layer.addChild(logo);
+
+
+    LTweenLite.to(bgAb,0.2,{alpha:1,ease:LEasing.None.easeIn}).to(bgAb,0.2,{alpha:0,ease:LEasing.None.easeIn}).to(bgAb,0.2,{alpha:1,delay:1,ease:LEasing.None.easeIn}).to(bgAb,0.2,{alpha:0,ease:LEasing.None.easeIn}).to(bgAb,0.2,{alpha:1,ease:LEasing.None.easeIn}).to(bgAb,0.2,{alpha:0,ease:LEasing.None.easeIn}).to(bgAb,0.2,{alpha:1,ease:LEasing.None.easeIn,onComplete:function(){
+        typeC("txt1",function(){
+            typeC("txt2",function(){
+                LTweenLite.to(btnNext,0.8,{alpha:1,ease:LEasing.None.easeIn}).to(btnNext,0.8,{alpha:0,loop:true,ease:LEasing.None.easeIn}).to(btnNext,0.8,{alpha:1,loop:true,ease:LEasing.None.easeIn});
+            })
+        });
+    }});
+
+}
+
+function page2Init(){
+    page2Layer = new LSprite();
+    page2Layer.graphics.drawRect(0,"#fff100",[0,0,750,1217],true,"#000");
+    stage.addChild(page2Layer);
+}
+
+function page3Init(){
+    page3Layer = new LSprite();
+    page3Layer.graphics.drawRect(0,"#fff100",[0,0,750,1217],true,"#000");
+    stage.addChild(page3Layer);
+
+    var pic1 = new LBitmap(new LBitmapData(imglist["p3_pic1"]));
+    pic1.y = -10;
+    page3Layer.addChild(pic1);
+
+    var cover = new LSprite();
+    cover.graphics.drawRect(0,'#fff',[40,25,680,440]);
+
+    var dot = new LBitmap(new LBitmapData(imglist["icons"],0,400,90,144));
+    dot.x = 320;
+    dot.y = 190;
+    dot.scaleX = 0.66;
+    dot.scaleY = 0.66;
+    dot.mask = cover;
+    page3Layer.addChild(dot);
+
+    var rode = new LBitmap(new LBitmapData(imglist["icons"],0,1037,210,420));
+    rode.x = 190;
+    rode.y = 235;
+    rode.scaleX = 0.66;
+    rode.scaleY = 0.66;
+    rode.mask = cover;
+    page3Layer.addChild(rode);
+
+    var line = new LSprite();
+    line.pic = new LBitmap(new LBitmapData(imglist["icons"],0,0,20,400));
+    line.pic.x = -10;
+    line.pic.y = -400;
+    line.addChild(line.pic);
+    line.x = 185;
+    line.y = 490;
+    line.scaleX = 0.66;
+    line.scaleY = 0.66;
+    line.rotate = 40;
+    line.mask = cover;
+    page3Layer.addChild(line);
+
+    var exit = new LBitmap(new LBitmapData(imglist["icons"],0,656,120,70));
+    exit.x = 210;
+    exit.y = 335;
+    exit.scaleX = 0.66;
+    exit.scaleY = 0.66;
+    exit.mask = cover;
+    page3Layer.addChild(exit);
+
+    var info1 = new LBitmap(new LBitmapData(imglist["c_imgs"],0,1330,586,296));
+    info1.x = 360;
+    info1.y = 50;
+    info1.scaleX = 0.6;
+    info1.scaleY = 0.6;
+    page3Layer.addChild(info1);
+
+    /*
+        bottom
+    */
+    var pic2 = new LBitmap(new LBitmapData(imglist["p3_pic3"]));
+    pic2.y = 470;
+    page3Layer.addChild(pic2);
+
+    var car1 = new LBitmap(new LBitmapData(imglist["p3_pic2"],0,0,460,140));
+    car1.x = 20;
+    car1.y = 510;
+    page3Layer.addChild(car1);
+
+    var car2 = new LBitmap(new LBitmapData(imglist["p3_pic2"],0,146,460,140));
+    car2.x = 20;
+    car2.y = 656;
+    page3Layer.addChild(car2);
+
+    var car3 = new LBitmap(new LBitmapData(imglist["p3_pic2"],0,292,460,140));
+    car3.x = 20;
+    car3.y = 802;
+    page3Layer.addChild(car3);
+
+    var car4 = new LBitmap(new LBitmapData(imglist["p3_pic2"],472,0,240,432));
+    car4.x = 492;
+    car4.y = 510;
+    page3Layer.addChild(car4);
+
+    var info2 = new LBitmap(new LBitmapData(imglist["c_imgs"],0,329,220,254));
+    info2.x = 340;
+    info2.y = 610;
+    page3Layer.addChild(info2);
+
+    var info3 = new LBitmap(new LBitmapData(imglist["c_imgs"],0,806,262,196));
+    info3.x = 488;
+    info3.y = 1021;
+    page3Layer.addChild(info3);
+
+    var helicopter = new LBitmap(new LBitmapData(imglist["helicopter"]));
+    helicopter.x = 270;
+    helicopter.y = 370;
+    page3Layer.addChild(helicopter);
+}
+
+function page4Init(){
+    page4Layer = new LSprite();
+    page4Layer.graphics.drawRect(0,"#fff100",[0,0,750,1217],true,"#000");
+    stage.addChild(page4Layer);
+
+    var pic1 = new LBitmap(new LBitmapData(imglist["p4_pic1"]));
+    page4Layer.addChild(pic1);
+
+    var car = new Car();
+    car.x = 345;
+    car.y = 75;
+    page4Layer.addChild(car);
+
+    var pic2 = new LBitmap(new LBitmapData(imglist["p4_pic2"]));
+    pic2.y = 477;
+    page4Layer.addChild(pic2);
+}
+
+function page5Init(){
+    page5Layer = new LSprite();
+    page5Layer.graphics.drawRect(0,"#fff100",[0,0,750,1217],true,"#000");
+    stage.addChild(page5Layer);
+
+    var pic1 = new LBitmap(new LBitmapData(imglist["p5_pic1"],0,0,750,232));
+    page5Layer.addChild(pic1);
+
+    var pic2 = new LBitmap(new LBitmapData(imglist["p5_pic1"],0,235,750,470));
+    pic2.y = 235;
+    page5Layer.addChild(pic2);
+
+    var pic3 = new LBitmap(new LBitmapData(imglist["p5_pic1"],0,704,750,445));
+    pic3.y = 704;
+    page5Layer.addChild(pic3);
+
+    var info1 = new LBitmap(new LBitmapData(imglist["c_imgs"],0,0,182,88));
+    info1.x = 515;
+    info1.y = 840;
+    page5Layer.addChild(info1);
+}
+
+function page6Init(){
+    page6Layer = new LSprite();
+    page6Layer.graphics.drawRect(0,"#fff100",[0,0,750,1217],true,"#202020");
+    stage.addChild(page6Layer);
+
+    var pic1 = new LBitmap(new LBitmapData(imglist["p6_pic1"],40,0,574,470));
+    pic1.x = 40;
+    pic1.y = 155;
+    page6Layer.addChild(pic1);
+
+    var pic2 = new LBitmap(new LBitmapData(imglist["p6_pic1"],168,500,540,350));
+    pic2.x = 168;
+    pic2.y = 660;
+    page6Layer.addChild(pic2);
+
+    var hand = new LBitmap(new LBitmapData(imglist["icons"],31,7,82,90));
+    hand.x = 280;
+    hand.y = 560;
+    page6Layer.addChild(hand);
 }
