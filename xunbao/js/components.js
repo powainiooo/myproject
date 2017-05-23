eb847afaafb750c6fd9e4a552205021fb1e24d5a
zhangjian <powainiooo@163.com>
2017-05-23 17:33:51 +0800

    start

---
+++ xunbao/js/components.js
@@ -0,0 +1,235 @@
+//首页开始按钮
+function StartBtn(){
+    base(this,LSprite,[]);
+    var self = this;
+    self.x = 360;
+    self.y = 490;
+
+    self.name = new Zimg([imglist['icons'],730,0,273,91],-100,-150);
+    self.name.scaleX = 0.8;
+    self.name.scaleY = 0.8;
+    self.addChild(self.name);
+
+    self.btn = new ZRimg([imglist['icons'],0,155,190,193],-85,-86);
+    self.addChild(self.btn);
+    self.btn.addEventListener(LMouseEvent.MOUSE_DOWN,function(){
+        Zclick.call(self.btn);
+    })
+}
+
+//经验条
+function Lvline(lv){
+    base(this,LSprite,[]);
+    var self = this;
+    self.x = 25;
+    self.y = 5;
+
+    var bg = new Zimg([imglist['icons'],0,0,151,46]);
+    self.addChild(bg);
+
+    self.lineCover = new LSprite();
+    self.lineCover.graphics.drawRoundRect(1,'#f00',[0,0,105,18,10]);
+    self.lineCover.x = 41;
+    self.lineCover.y = 15;
+    self.lineCover.scaleX = 0.5;
+
+    var line = new Zimg([imglist['icons'],154,0,105,18],41,15);
+    line.mask = self.lineCover;
+    self.addChild(line);
+
+    self.txt = new LTextField();
+    self.txt.text = 'Lv.'+lv;
+    self.txt.x = 160;
+    self.txt.y = 5;
+    self.txt.size = 28;
+    self.txt.weight = 'bold';
+    self.txt.font = '微软雅黑';
+    self.txt.color = '#ffdf26';
+    self.addChild(self.txt);
+}
+
+//钻石条
+function DiamondLine(txt){
+    base(this,LSprite,[]);
+    var self = this;
+    self.graphics.drawRoundRect(2,'#3e7694',[0,0,166,36,20],true,'#173e5f');
+    self.x = 255;
+    self.y = 10;
+
+    var icon = new Zimg([imglist['icons'],236,20,45,38]);
+    self.addChild(icon);
+
+    self.txt = new LTextField();
+    self.txt.text = txt;
+    self.txt.x = 90;
+    self.txt.y = 4;
+    self.txt.size = 22;
+    self.txt.font = '微软雅黑';
+    self.txt.color = '#fff';
+    self.txt.textAlign = 'center';
+    self.addChild(self.txt);
+
+    self.btn = new LButton(new Zimg([imglist['icons'],193,20,40,40],140,0));
+    self.addChild(self.btn);
+}
+
+//提现条
+function MoneyLine(txt){
+    base(this,LSprite,[]);
+    var self = this;
+    self.graphics.drawRoundRect(2,'#3e7694',[0,0,230,36,20],true,'#173e5f');
+    self.x = 480;
+    self.y = 10;
+
+    var icon = new Zimg([imglist['icons'],288,3,44,50],0,-10);
+    self.addChild(icon);
+
+    self.txt = new LTextField();
+    self.txt.text = txt+'元';
+    self.txt.x = 110;
+    self.txt.y = 4;
+    self.txt.size = 22;
+    self.txt.font = '微软雅黑';
+    self.txt.color = '#fff';
+    self.txt.textAlign = 'center';
+    self.addChild(self.txt);
+
+    self.btn = new LButton(new Zimg([imglist['icons'],336,0,82,44],180,-5));
+    self.addChild(self.btn);
+}
+
+//个人信息
+function Persondata(){
+    base(this,LSprite,[]);
+    var self = this;
+    self.graphics.drawRoundRect(0,'#000',[0,0,106,106,12],true,'#4692a6');
+    self.x = 20;
+    self.y = 97;
+
+    var cover = new LSprite();
+    cover.graphics.drawRoundRect(0,'#000',[4,4,98,98,12]);
+    self.pic = new Zimg([imglist['person']],4,4);
+    self.pic.mask = cover;
+    self.addChild(self.pic);
+
+    var name = new LTextField();
+    name.text = '大脸猫爱吃鱼~';
+    name.size = 24;
+    name.color = '#a5efff';
+    name.font = '微软雅黑';
+    name.x = 110;
+    name.y = 30;
+    self.addChild(name);
+
+    var icon = new Zimg([imglist['icons'],153,19,38,40],90,65);
+    self.addChild(icon);
+
+    var age = new LTextField();
+    age.text = 13;
+    age.size = 28;
+    age.color = '#ffe123';
+    age.font = '微软雅黑';
+    age.x = 150;
+    age.y = 70;
+    self.addChild(age);
+}
+
+//首页功能点
+function HomeFunc(txt,x,y){
+    base(this,LSprite,[]);
+    var self = this;
+    self.graphics.drawArc(4,'#5ea1b2',[0,0,52,0,Math.PI*2],true,'#324973');
+    self.x = x;
+    self.y = y;
+
+    if(txt == '邀请好友'){
+        self.icon = new LButton(new Zimg([imglist['icons'],525,4,93,90],-42,-45));
+    }else if(txt == '活动'){
+        self.icon = new LButton(new Zimg([imglist['icons'],426,0,90,90],-46,-45));
+    }else if(txt == '帮助'){
+        self.icon = new LButton(new Zimg([imglist['icons'],86,46,68,68],-34,-34));
+        self.icon.scaleX = 1.2;
+        self.icon.scaleY = 1.2;
+    }
+    self.addChild(self.icon);
+
+    var name = new LTextField();
+    name.text = txt;
+    name.size = 30;
+    name.color = '#fff';
+    name.font = '黑体';
+    name.weight = 'bold';
+    name.stroke = true;
+    name.lineWidth = 6;
+    name.lineColor = '#001625';
+    name.x = -(name.getWidth())/2;
+    name.y = 40;
+    self.addChild(name);
+}
+
+//首页底部功能点
+function HomeBottomFunc(txt,x,y){
+    base(this,LSprite,[]);
+    var self = this;
+    self.x = x;
+    self.y = y;
+
+    if(txt == '好友'){
+        self.icon = new LButton(new Zimg([imglist['icons'],267,60,131,133]));
+    }else if(txt == '排行榜'){
+        self.icon = new LButton(new Zimg([imglist['icons'],269,197,131,133]));
+    }else if(txt == '喂养'){
+        self.icon = new LButton(new Zimg([imglist['icons'],405,94,131,133]));
+    }else if(txt == '消息'){
+        self.icon = new LButton(new Zimg([imglist['icons'],537,96,131,133]));
+    }
+    self.icon.scaleX = 0.74;
+    self.icon.scaleY = 0.74;
+    self.addChild(self.icon);
+
+    var name = new LTextField();
+    name.text = txt;
+    name.size = 30;
+    name.color = '#fff';
+    name.font = '黑体';
+    name.weight = 'bold';
+    name.stroke = true;
+    name.lineWidth = 6;
+    name.lineColor = '#001625';
+    name.x = 49;
+    name.y = 90;
+    name.textAlign = 'center';
+    self.addChild(name);
+}
+
+//男猫
+function Male(){
+    base(this,LSprite,[]);
+    var self = this;
+    self.x = 350;
+    self.y = 880;
+
+    var list = LGlobal.divideCoordinate(2720, 453, 1, 8);
+    var data = new LBitmapData(imglist["male"]);
+    self.pic = new LAnimationTimeline(data, list);
+    self.pic.speed = 5;
+    self.pic.x = -170;
+    self.pic.y = -226;
+    self.addChild(self.pic);
+}
+
+//男猫
+function Female(){
+    base(this,LSprite,[]);
+    var self = this;
+    self.x = 350;
+    self.y = 880;
+
+    var list = LGlobal.divideCoordinate(3280, 481, 1, 8);
+    var data = new LBitmapData(imglist["female"]);
+    self.pic = new LAnimationTimeline(data, list);
+    self.pic.speed = 5;
+    self.pic.x = -205;
+    self.pic.y = -240;
+    self.addChild(self.pic);
+}
---
+++ xunbao/js/page.js
@@ -0,0 +1,164 @@
+﻿var stage;
+
+var loadinglist = {};
+var loadingData = new Array(
+    {name:"L_loading",path:imgsrc+"./images/loading_cover.png"},
+    {name:"L_line",path:imgsrc+"./images/loading_line.png"}
+);
+
+var imglist = {};
+var imgData = new Array(
+    {name:"bg1",path:imgsrc+"./images/bg1.jpg"},
+    {name:"bg2",path:imgsrc+"./images/bg2.jpg"},
+    {name:"select-female",path:imgsrc+"./images/select-female.png"},
+    {name:"select-male",path:imgsrc+"./images/select-male.png"},
+    {name:"person",path:imgsrc+"./images/person.jpg"},
+    {name:"icons",path:imgsrc+"./images/icons.png"},
+    {name:"male",path:imgsrc+"./images/male.png"},
+    {name:"female",path:imgsrc+"./images/female.png"}
+);
+
+function main(){
+    LGlobal.setDebug(true);
+    //全屏操作
+    LGlobal.align = LStageAlign.TOP_MIDDLE;
+    LGlobal.stageScale = LStageScaleMode.EXACT_FIT;
+    LSystem.screen(LStage.FULL_SCREEN);
+
+    //添加舞台
+    stage = new LSprite();
+    addChild(stage);
+
+    addChild(new FPS());
+
+    //添加加载层
+    LLoadManage.load(
+        loadingData,
+        function(progress){
+
+        },
+        function(result){
+            loadinglist = result;
+            loading_gameData();
+        }
+    );
+}
+
+function loading_gameData(){
+    //添加加载层
+    var loadinglayer = new LoadingDoufen();
+    stage.addChild(loadinglayer);
+    LLoadManage.load(
+        imgData,
+        function(progress){
+            loadinglayer.setProgress(progress);
+        },
+        function(result){
+            imglist = result;
+            stage.removeChild(loadinglayer);
+            loadinglayer = null;
+            selectPage();
+        }
+    );
+}
+
+function selectPage(){
+    selectLayer = new LSprite();
+    stage.addChild(selectLayer);
+
+    var bg = new Zimg([imglist['bg2']]);
+    selectLayer.addChild(bg);
+
+    var female = new ZRimg([imglist['select-female']],0,0);
+    female.x = 13;
+    female.y = 140;
+    selectLayer.addChild(female);
+
+    var btnFemale = new ZRimg([imglist['icons'],408,232,244,80],-122,-35);
+    btnFemale.x = 580;
+    btnFemale.y = 350;
+    female.addChild(btnFemale);
+    btnFemale.addEventListener(LMouseEvent.MOUSE_DOWN,function(){
+        Zclick.call(btnFemale,function(){
+            homePage('female');
+        });
+    });
+
+    var male = new ZRimg([imglist['select-male']],0,0);
+    male.x = 13;
+    male.y = 610;
+    selectLayer.addChild(male);
+
+    var btnMale = new ZRimg([imglist['icons'],408,232,244,80],-122,-35);
+    btnMale.x = 580;
+    btnMale.y = 350;
+    male.addChild(btnMale);
+    btnMale.addEventListener(LMouseEvent.MOUSE_DOWN,function(){
+        Zclick.call(btnMale,function(){
+            homePage('male');
+        });
+    });
+}
+
+function homePage(sex){
+    homeLayer = new LSprite();
+    stage.addChild(homeLayer);
+
+    var bg = new Zimg([imglist['bg1']]);
+    homeLayer.addChild(bg);
+
+    var startBtn = new StartBtn();
+    homeLayer.addChild(startBtn);
+
+    var lvline = new Lvline(5);
+    homeLayer.addChild(lvline);
+
+    var diamondLine = new DiamondLine(5);
+    homeLayer.addChild(diamondLine);
+
+    var moneyLine = new MoneyLine(5);
+    homeLayer.addChild(moneyLine);
+
+    var persondata = new Persondata();
+    homeLayer.addChild(persondata);
+
+    var btnInvite = new HomeFunc('邀请好友',395,150);
+    homeLayer.addChild(btnInvite);
+
+    var btnActiv = new HomeFunc('活动',535,150);
+    homeLayer.addChild(btnActiv);
+
+    var btnHelp = new HomeFunc('帮助',675,150);
+    homeLayer.addChild(btnHelp);
+
+    var btnGZH = new LButton(new Zimg([imglist['icons'],3,49,80,102]));
+    btnGZH.x = 25;
+    btnGZH.y = 225;
+    homeLayer.addChild(btnGZH);
+
+    var btnFeedList = new LButton(new Zimg([imglist['icons'],196,64,64,220]));
+    btnFeedList.y = 955;
+    homeLayer.addChild(btnFeedList);
+
+    var btnFriend = new HomeBottomFunc('好友',90,1200);
+    homeLayer.addChild(btnFriend);
+
+    var btnRank = new HomeBottomFunc('排行榜',245,1200);
+    homeLayer.addChild(btnRank);
+
+    var btnFeed = new HomeBottomFunc('喂养',400,1200);
+    homeLayer.addChild(btnFeed);
+
+    var btnMsg = new HomeBottomFunc('消息',555,1200);
+    homeLayer.addChild(btnMsg);
+
+    if(sex == 'male'){
+        var male = new Male();
+    }else if(sex == 'female'){
+        var male = new Female();
+    }
+    homeLayer.addChild(male);
+
+    var transParams = {type:LTransition.Curtain,dimension:1,duration:2,direction:LTransition.IN,easing:Strong.easeOut};
+    LTransitionManager.start(homeLayer,transParams);
+}
